fb5530a1eb206998e1f6051f3c28a8b0cd5cb7bc
MahadevanSrinivasan <macsdev@gmail.com>
2012-06-19 09:20:37 -0700

    first commit

---
+++ benchmark.py
@@ -0,0 +1,47 @@
+#! /usr/bin/env python
+
+from collections import deque
+import utilities, time
+import cPickle as pickle
+
+def breadth_first_search(graph, node, num_nodes):
+    """
+    Does a breadth-first search of the graph starting at the node.
+    Returns the first num_nodes nodes (excluding direct neighbors)
+    """
+    neighbors = set(graph[node])
+    looked_at = set(graph[node])
+    looked_at.add(node)
+    visited = []
+    queue = deque(graph[node])
+    while queue and len(visited)<num_nodes:
+        next_node = queue.popleft()
+        if next_node not in neighbors:
+            visited.append(next_node)
+        queue.extend(n for n in graph[next_node] if n not in looked_at)
+        looked_at.update(graph[next_node])
+    
+    return visited
+
+def bfs_benchmark(train_file, test_file, submission_file, num_predictions):
+    """
+    Runs the breadth-first search benchmark.
+    """
+    graph = utilities.read_graph(train_file)
+    output = open('graph.pkl', 'wb')
+    pickle.dump(graph, output, protocol=2)
+    output.close()
+    test_nodes = utilities.read_nodes_list(test_file)
+    start_time = time.time()
+    test_predictions = [breadth_first_search(graph, node, num_predictions)
+                        for node in test_nodes]
+    print time.time() - start_time, "seconds"
+    utilities.write_submission_file(submission_file, 
+                                    test_nodes, 
+                                    test_predictions)
+
+if __name__=="__main__":
+    bfs_benchmark("../Data/train.csv",
+                  "../Data/test.csv",
+                  "../Submissions/bfs_benchmark_updated.csv",
+                  10)
